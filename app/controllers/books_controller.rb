2308f33cddbe5bc94a764b23374504fa0e863c98
EC2 Default User <m2.tmk@ezweb.ne.jp>
2021-08-15 13:02:42 +0000

    step22

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,10 @@
 class BooksController < ApplicationController
   def index
-    @lists = Book.all
+    @books = Book.all
   end
 
   def show
-    @list = Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -18,8 +18,21 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.permit(:title, :body)
