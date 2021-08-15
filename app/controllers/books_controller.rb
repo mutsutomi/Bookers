9d60547c4987c330e40a51b85775d9b9fd6ba0f8
EC2 Default User <m2.tmk@ezweb.ne.jp>
2021-08-15 19:59:59 +0000

    step31

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,10 +13,11 @@ class BooksController < ApplicationController
   end
 
   def create
-    if@book = Book.new(book_params)
-    @book.save
+    @book = Book.new(book_params)
+    if@book.save
       redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
+      @books = Book.all
      render :index
     end
   end
@@ -26,11 +27,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-      redirect_to book_path(book.id), notice: 'Book was successfully updated.'
-
-
+    @book = Book.find(params[:id])
+    if@book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
