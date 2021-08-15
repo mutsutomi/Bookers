7e252d93ab863fbaa78fc3d1375198ecc4d7055c
EC2 Default User <m2.tmk@ezweb.ne.jp>
2021-08-15 20:04:28 +0000

    addSpaceAfterIf

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    if@book.save
+    if @book.save
       redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
       @books = Book.all
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if@book.update(book_params)
+    if @book.update(book_params)
       redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
     else
       render :edit
