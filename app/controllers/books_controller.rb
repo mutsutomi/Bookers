c3daec7438e5a2007c653e761027eb242961791d
EC2 Default User <m2.tmk@ezweb.ne.jp>
2021-08-15 11:30:55 +0000

    step17

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,27 @@
 class BooksController < ApplicationController
   def index
+    @lists = Book.all
   end
 
   def show
+    @list = Book.find(params[:id])  
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
